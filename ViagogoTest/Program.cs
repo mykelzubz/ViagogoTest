04c60a79f66a3d340df6e6d3577cac79b7d1ae5d
Michael <onyirims@yahoo.com>
2022-07-19 18:01:18 +0100

    completed solution

--- ViagogoTest/Program.cs
+++ ViagogoTest/Program.cs
@@ -9,6 +9,8 @@ namespace Viagogo
     {
         public string Name{ get; set; }
         public string City{ get; set; }
+        public int Distance { get; set; }
+        public int? Price { get; set; }
     }
 
     public class Customer
@@ -20,6 +22,8 @@ namespace Viagogo
 
     public class Solution
     {
+        private static Dictionary<string, int> cachedDistances = new Dictionary<string, int>();
+        
         static void Main(string[] args)
         {
             var events = new List<Event>{
@@ -40,8 +44,14 @@ namespace Viagogo
             // var query = from result in customer
             //     where result.Contains("New York")
             //     select result;
+            
+            
             // 1. TASK
-            foreach(var item in events)
+            
+            //Get eligible events
+            var eligibleEvents = events.Where(x => x.City.Equals(customer.City));
+            
+            foreach(var item in eligibleEvents)
             {
                 AddToEmail(customer, item);
             }
@@ -49,7 +59,75 @@ namespace Viagogo
             * We want you to send an email to this customer with all events in their city
             * Just call AddToEmail(customer, event) for each event you think they should get
             */
+            
+            // 2. SEND EMAIL FOR 5 CLOSEST EVENTS
+            var fiveClosest = events.Select(e => new Event { Name = e.Name, City = e.City, Distance = GetDistance(customer.City, e.City) })
+                                                            .OrderBy(e => e.Distance)
+                                                            .Take(5);
+            
+            foreach (var item in fiveClosest)
+            {
+                AddToEmail(customer, item);
+            }
+            
+            // 3. If the GetDistance method is an API call which could fail or is too expensive, OptimizedGetDistance method created below
+            var fiveClosestOptimized = events.Select(e => new Event { Name = e.Name, City = e.City, Distance = OptimizedGetDistance(customer.City, e.City) })
+                                                                .OrderBy(e => e.Distance)
+                                                                .Take(5);
+            
+            foreach (var item in fiveClosestOptimized)
+            {
+                AddToEmail(customer, item);
+            }
+            
+            
+            // 4. If the GetDistance method can fail, we don't want the process to fail.
+            
+                //SAME AS SOLUTION 3 ABOVE
+                
+            
+            // 5. Sort by other fields (e.g price)
+            var furtherSortedEvents = events.Select(e => new Event { Name = e.Name, City = e.City, Distance = OptimizedGetDistance(customer.City, e.City) })
+                                                            .OrderBy(e => e.Distance)
+                                                            .ThenBy(e => e.Price)
+                                                            .Take(5);
+
+            foreach (var item in furtherSortedEvents)
+            {
+                AddToEmail(customer, item, item.Price);
+            }
         }
+
+        static int OptimizedGetDistance(string fromCity, string toCity)
+        {
+            
+            if (fromCity.Equals(toCity))
+                return 0;
+            
+            var key = fromCity + "_" + toCity;
+
+            if (cachedDistances.TryGetValue(key, out var cachedDistance))
+            {
+                return cachedDistance;
+            }
+            
+            try
+            {
+                var distance = GetDistance(fromCity, toCity);
+                cachedDistances.Add(key, distance);
+
+                return distance;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+            //Return max value since the GetDistance method failed
+            return int.MaxValue;
+        }
+        
+        
         
         // You do not need to know how these methods work
         static void AddToEmail(Customer c, Event e, int? price = null)
